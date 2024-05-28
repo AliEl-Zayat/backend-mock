247f310f99f9b6697db58a93c8a4b5c4f6633f8f
Ali El-Zayat <alielzayat07@gmail.com>
2024-05-28 14:13:26 +0300

    mocker server ready to deploy

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Backend server is running");
+});
+
+app.post("/api/v1/users/token", (req, res) => {
+  console.log(req.body);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
